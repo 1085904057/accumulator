385e5e0589a2909a6343260cc867cf3be23c9560
wangrubin1 <wangrubin3@jd.com>
2022-07-11 09:25:17 +0800

    first edition

---
+++ src/test/java/com/spring/accumulator/accumulator/AccumulatorApplicationTests.java
@@ -0,0 +1,18 @@
+package com.spring.accumulator.accumulator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = AccumulatorApplication.class)
+class AccumulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
